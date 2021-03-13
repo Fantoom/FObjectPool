57b3c6f523b62fdd519161436b60d5d9297064f9
Fantoom <serge.vardanyan00@mail.ru>
2021-03-13 15:31:02 +0400

    Initial

---
+++ FObjectPool/ObjectPool.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FObjectPool
+{
+	public class ObjectPool<TObject>
+	{
+		private Queue<TObject> pool;
+		public event Action<TObject> OnAdd = delegate { };
+		public event Action<TObject> OnGet = delegate { };
+		private Func<TObject> objectCreation;
+		private int maxCount = int.MaxValue;
+
+		public int MaxCount
+		{
+			get { return MaxCount; }
+			set { maxCount = value > MaxCount ? value : MaxCount; }
+		}
+
+		public ObjectPool()
+		{
+			pool = new Queue<TObject>();
+		}
+
+		public ObjectPool(IEnumerable<TObject> initialObjects)
+		{
+			pool = new Queue<TObject>(initialObjects);
+		}
+
+		public ObjectPool(IEnumerable<TObject> initialObjects, int maxCount)
+		{
+			MaxCount = maxCount;
+			pool = new Queue<TObject>(initialObjects);
+		}
+
+		public ObjectPool(Func<TObject> objectCreation)
+		{
+			if (objectCreation is null)
+			{
+				throw new ArgumentNullException(nameof(objectCreation));
+			}
+
+			this.objectCreation = objectCreation;
+			pool = new Queue<TObject>();
+		}
+
+		public ObjectPool(Func<TObject> objectCreation, int maxCount)
+		{
+			if (objectCreation is null)
+			{
+				throw new ArgumentNullException(nameof(objectCreation));
+			}
+
+			MaxCount = maxCount;
+			this.objectCreation = objectCreation;
+			pool = new Queue<TObject>();
+		}
+
+		public ObjectPool(Func<TObject> objectCreation, IEnumerable<TObject> initialObjects)
+		{
+			if (objectCreation is null)
+			{
+				throw new ArgumentNullException(nameof(objectCreation));
+			}
+
+			MaxCount = maxCount;
+			this.objectCreation = objectCreation;
+			pool = new Queue<TObject>(initialObjects);
+		}
+
+		public ObjectPool(Func<TObject> objectCreation, IEnumerable<TObject> initialObjects, int maxCount)
+		{
+			if (objectCreation is null)
+			{
+				throw new ArgumentNullException(nameof(objectCreation));
+			}
+			if (initialObjects.Count() < maxCount)
+			{
+				throw new ArgumentException("Maximum items count can not be less than initial objects count", nameof(maxCount));
+			}
+
+			MaxCount = maxCount;
+			this.objectCreation = objectCreation;
+			pool = new Queue<TObject>(initialObjects);
+		}
+
+		public void FillPool(int count)
+		{
+			if (objectCreation is null)
+			{
+				throw new Exception($"{nameof(objectCreation)} is not set");
+			}
+
+			if (count > maxCount)
+			{
+				throw new ArgumentException("Count can not be greater than maximum items count", nameof(count));
+			}
+
+			foreach (var item in Enumerable.Range(0, count).Select(x => objectCreation()))
+			{
+				AddObject(item);
+			}
+		}
+
+		public void FillPool(IEnumerable<TObject> objects)
+		{
+			if (objects.Count() > maxCount)
+			{
+				throw new ArgumentException("Objects count can not be greater than max count", nameof(objects));
+			}
+			foreach (var item in objects)
+			{
+				AddObject(item);
+			}
+		}
+
+		public ObjectPoolItem<TObject> GetObject()
+		{
+			TObject obj;
+			if (pool.Count == 0)
+			{
+				obj = objectCreation();
+			}
+            else 
+			{
+				obj = pool.Dequeue();
+			}
+			OnGet(obj);
+			return new ObjectPoolItem<TObject>(this, obj);
+		}
+
+		public bool AddObject(TObject objectToAdd)
+		{
+			if (!pool.Contains(objectToAdd) && pool.Count < maxCount)
+			{
+				pool.Enqueue(objectToAdd);
+				OnAdd(objectToAdd);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ FObjectPool/ObjectPoolItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FObjectPool
+{
+	public class ObjectPoolItem<TItem> : IDisposable
+	{
+		public ObjectPool<TItem> Pool { get; private set; }
+		public TItem item { get; private set; }
+
+		public TItem Item
+		{
+			get
+			{
+				if (!disposed)
+					return item;
+				else
+					throw new ObjectDisposedException(nameof(Item), "Object is reverted back to pool");
+			}
+			private set => item = value;
+		}
+
+		private bool disposed = false;
+
+		public ObjectPoolItem(ObjectPool<TItem> pool, TItem item)
+		{
+			Pool = pool;
+			Item = item;
+		}
+
+		public void BackToPool()
+		{
+			Dispose();
+		}
+
+		public void Dispose()
+		{
+			Pool.AddObject(Item);
+			disposed = true;
+			item = default;
+		}
+	}
+}
